3d969d983398999f653fe448d372f16603ff4256
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-02-23 23:41:54 -0300

    Versão com a camada Model criada.

---
+++ lojadegames/src/main/java/com/generation/lojadegames/model/CategoriaConsole.java
@@ -0,0 +1,56 @@
+package com.generation.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name="tb_console")
+public class CategoriaConsole {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="Este campo obrigatório, de 5 à 30 caracteres.")
+	@Size(min=5, max=30, message="Campo obrigatório, de 5 à 30 caracteres.")
+	private String console;
+	
+	@NotBlank(message="Este campo obrigatório.")
+	@Size(min=4, max=4, message="Este campo deve conter 4 dígitos")
+	private int anoDeLancamento;
+	
+	
+	@NotBlank(message="Este campo obrigatório, de 5 à 30 caracteres.")
+	@Size(max=15, message="Campo obrigatório, máximo de 15 caracteres.")
+	private String empresa;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public int getAnoDeLancamento() {
+		return anoDeLancamento;
+	}
+
+	public void setAnoDeLancamento(int anoDeLancamento) {
+		this.anoDeLancamento = anoDeLancamento;
+	}
+	
+}
